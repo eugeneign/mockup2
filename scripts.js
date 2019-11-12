0846eb3d3762bac21ed9e20eb256076428ef8946
eugeneign <49946057+eugeneign@users.noreply.github.com>
2019-11-12 15:39:06 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,40 @@
+$(document).ready
+
+
+$('.header__nav-search').on("click", function (e) {
+  e.preventDefault();
+  $('.header__search').toggleClass('header__search-is-opened')
+});
+
+$('.header__search-is-closed').on("click", function (e) {
+  e.preventDefault();
+  $('.header__search').toggleClass('header__search-is-opened')
+});
+
+$('.header__mobile').on("click", function (e) {
+  e.preventDefault();
+  $('.header__mobileNav').toggleClass('header__mobileNav-is-opened')
+});
+
+$('.header__mobileNav-is-closed').on("click", function (e) {
+  e.preventDefault();
+  $('.header__mobileNav').toggleClass('header__mobileNav-is-opened')
+});
+
+$('.header__mobileNav-itemSearch').on("click", function (e) {
+  e.preventDefault();
+  $('.header__search').toggleClass('header__search-is-opened');
+  $('.header__mobileNav').toggleClass('header__mobileNav-is-opened');
+});
+
+$('.hero').slick({
+  infinite: true,
+  arrows: false,
+  fade: true,
+  cssEase: 'linear',
+  prevArrow: false,
+  nextArrow: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+
+});
